d8da24f3aca38585e80220ed7306c5db3044f5ea
NathenDeveloper <nuggetskaggs2012@gmail.com>
2026-08-08 04:01:27 -0400

    Delete Native.hpp

--- Native.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-
-namespace NativeBindings {
-    inline void registerCoreBindings() {
-        // Sandboxed environment: No system calls, file access, or unsafe hooks allowed.
-    }
-}
